cfe0ac24ca0efd18e33b555fd01ef28e9d1a145e
shubhamKhanwalkar <64422890+shubhamKhanwalkar@users.noreply.github.com>
2020-06-07 17:45:12 +0530

    changed bin.js

--- Bin.js
+++ Bin.js
@@ -1,8 +1,44 @@
-class Bin extends BaseClass{
+class Bin{
   constructor(x, y, width, height) {
-    super(x,y,width,height);
-  
-   
+    var options = {
+      isStatic : true 
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;  
+    this.image = loadImage("dustbingreen.png");
+    World.add(world, this.body);
   }
+  display(){
+    var pos =this.body.position;
+    push();
+    translate(pos.x, pos.y);
+    imageMode(CENTER);
+    fill("yellow");
+    image(this.image,0 ,-90 , 170, 200);
+   // rect(0, 0, this.width, this.height);
+    pop();
+  } 
+  
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- sketch.js
+++ sketch.js
@@ -4,23 +4,20 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
-function preLoad() {
-  ball_img = loadImage ("dustbingreen.png"); 
-}
+
 
 function setup() {
   createCanvas(1200,800);
   engine = Engine.create();
   world = engine.world;
-  d1 = new Bin(870,430,20,180);
-  d2 = new Bin(945,510,130,10);
-  d3 = new Bin(1020,430,20,180);
+  d1 = new BaseClass(880,430,20,190);
+  d3 = new BaseClass(1016,430,20,190);
+  d2 = new Bin(948,510,130,10);
   ball = new Ball(100,100,45,45);
   shooter = new Shooter(ball.body,{x:200,y:200});
-  box = createSprite (200,200,100,100);
+ 
   ground = new Ground(width/2,530,width,30);
-  box.addImage(ball_img);
-
+ 
 }
 
 
@@ -30,9 +27,9 @@ function draw() {
   background(255,255,2550);  
   ball.display();
   d1.display();
-  d2.display();
   d3.display();
- ground.display();
+  d2.display(); 
+  ground.display();
  
   shooter.display();
  // console.log(BaseClass);
